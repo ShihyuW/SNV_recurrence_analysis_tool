fa31585e0952d1582c32257b12c44aef816acbb0
Yen Ling Peng <s0890003@gmail.com>
2020-10-30 15:02:10 +0800

    modify the sample_list

--- germline_snv_recurrence.py
+++ germline_snv_recurrence.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Oct 28 16:03:13 2020
 
-@author: Jacob-Lab
+@author: YenlingPeng
 """
 
 import pandas as pd
@@ -16,8 +16,8 @@ import collections as col
 #union_variant = union_variant[["Chr", "Start", "End", "Func.refGene", "Gene.refGene", "AAChange.refGene" ,"AF_eas.2", "avsnp150"]]
 
 # MDF016 & MDF048 family member ANNOVAR file
-Sample_list = ["MDD0026_hg38_annotation","MDD0027_hg38_annotation","MDD0131_hg38_annotation","MDD0132_hg38_annotation","MDD0188_hg38_annotation"]
-ID_list = ["MDD0026", "MDD0027","MDD0131","MDD0132","MDD0188"]
+Sample_list = ["germline_test01","germline_test02"]
+
 # generate info tags for N/T
 # Otherinfo12 (there are three format...)
 # GT:AD:AF:DP:F1R2:F2R1:GQ:PL:GP:PRI:SB:MB:PS
@@ -42,17 +42,6 @@ for i in Sample_list:
     final_Annotation_file=pd.concat([file,df1],axis=1)
     final_Annotation_file.to_excel(f"{i}.xlsx", index=False)
 
-#Calculate VAF
-def calculate_AF_T_N(excel_file):
-    val_list=[]
-    df=pd.read_excel(excel_file)
-    for i in range(len(df)):
-        val=df.loc[i]["GQ_N"]-df.loc[i]["AF_N"]
-        val_list.append(val)
-    valdf=pd.DataFrame(val_list,columns=["GQ_N-AF_N"])
-    finaldf=pd.concat([df,valdf],axis=1)
-    return finaldf
-
 # create index
 def create_idx(df):
     idx_list=[]
@@ -72,11 +61,11 @@ for i in Sample_list:
     create_idx(pd.read_excel(f"{i}.xlsx")).to_excel(f"Indexed_{i}.xlsx", index=False)
 
 # Process union file
-pd.read_csv("union_of_variants.txt",sep="\t").to_excel("union_of_variants.xlsx", index=False)
-create_idx(pd.read_excel("union_of_variants.xlsx")).to_excel("Indexed_union_of_variants.xlsx", index=False)
+pd.read_csv("germline_union_of_variants.txt",sep="\t").to_excel("germline_union_of_variants.xlsx", index=False)
+create_idx(pd.read_excel("germline_union_of_variants.xlsx")).to_excel("Indexed_germline_union_of_variants.xlsx", index=False)
 
 # Generate PASSed and 3 filtered array by Idx
-U=pd.read_excel("Indexed_union_of_variants.xlsx", index_col="Idx")[["Chr","Start","End","Func.refGene", "ExonicFunc.refGene" ,"Gene.refGene", "AAChange.refGene" ,"AF_eas.1", "avsnp150"]]
+U=pd.read_excel("Indexed_germline_union_of_variants.xlsx", index_col="Idx")[["Chr","Start","End","Func.refGene", "ExonicFunc.refGene" ,"Gene.refGene", "AAChange.refGene" ,"AF_eas.1", "avsnp150"]]
 
 for i in Sample_list:
     S=pd.read_excel(f"Indexed_{i}.xlsx",index_col="Idx")
